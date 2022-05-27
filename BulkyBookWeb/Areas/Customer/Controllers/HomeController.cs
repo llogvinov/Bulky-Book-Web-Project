3e567d0ccba7bb692a708fa83e5c0918ca82b0d2
Lev Logvinov <llogvinov356@gmail.com>
2022-05-27 21:43:57 +0300

    Add items to existing Shopping Cart

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -47,7 +47,19 @@ namespace BulkyBookWeb.Controllers
             var claim = claimsIdentitiy.FindFirst(ClaimTypes.NameIdentifier);
             shoppingCart.ApplicationUserId = claim.Value;
 
-            _unitOfWork.ShoppingCart.Add(shoppingCart);
+
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(
+                u => u.ApplicationUserId == claim.Value && u.ProductId == shoppingCart.ProductId);
+
+            if (cartFromDb == null)
+            {
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.IncrementCount(cartFromDb, shoppingCart.Count);
+            }
+
             _unitOfWork.Save();
 
             return RedirectToAction(nameof(Index));
