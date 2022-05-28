79e9c58c99fd80d8eb77134c39240929ecbd1b05
Lev Logvinov <llogvinov356@gmail.com>
2022-05-28 12:53:26 +0300

    Load Shopping Cart List in Get Action

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingCartVM ShoppingCartVM;
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentitiy = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentitiy.FindFirst(ClaimTypes.NameIdentifier);
+
+            ShoppingCartVM = new ShoppingCartVM()
+            {
+                ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value,
+                includeProperties: "Product")
+            };
+
+            return View(ShoppingCartVM);
         }
     }
 }
